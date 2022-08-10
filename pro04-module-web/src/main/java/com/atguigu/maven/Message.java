0576a5e7701252b96a302f3c661f72991e1217ea
codernine <2961473141@qq.com>
2022-08-10 15:39:34 +0800

    master commit2

--- pro04-module-web/src/main/java/com/atguigu/maven/Message.java
+++ pro04-module-web/src/main/java/com/atguigu/maven/Message.java
@@ -16,14 +16,6 @@ public class Message {
         System.out.println("hello git1");
         System.out.println("hello git2");
         System.out.println("hello git3");
-        System.out.println("hello git4");
-        System.out.println("hello git5");
-        System.out.println("hello git6");
-        System.out.println("hello git7");
-        System.out.println("hello git7");
-        System.out.println("hello git7");
-        System.out.println("hello git7");
-        System.out.println("hello git7");
-        System.out.println("hello git7");
+        System.out.println("marster git4");
     }
 }
