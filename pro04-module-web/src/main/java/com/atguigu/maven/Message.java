cb9f6fc7c4c91a059c912cd9fe90874c585f047e
codernine <2961473141@qq.com>
2022-08-10 15:38:38 +0800

    hot-fix commit2

--- pro04-module-web/src/main/java/com/atguigu/maven/Message.java
+++ pro04-module-web/src/main/java/com/atguigu/maven/Message.java
@@ -14,16 +14,7 @@ public class Message {
 
     public static void main(String[] args) {
         System.out.println("hello git1");
-        System.out.println("hello git2");
-        System.out.println("hello git3");
-        System.out.println("hello git4");
-        System.out.println("hello git5");
-        System.out.println("hello git6");
-        System.out.println("hello git7");
-        System.out.println("hello git7");
-        System.out.println("hello git7");
-        System.out.println("hello git7");
-        System.out.println("hello git7");
-        System.out.println("hello git7");
+        System.out.println("hot-fix git2");
+
     }
 }
