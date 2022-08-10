99e536ffd80e984d13da79ad48e6a4219ffeb799
codernine <2961473141@qq.com>
2022-08-10 15:22:34 +0800

    second commit

--- pro04-module-web/src/main/java/com/atguigu/maven/Message.java
+++ pro04-module-web/src/main/java/com/atguigu/maven/Message.java
@@ -13,6 +13,9 @@ public class Message {
     }
 
     public static void main(String[] args) {
-        System.out.println("hello git");
+        System.out.println("hello git1");
+        System.out.println("hello git2");
+        System.out.println("hello git3");
+        System.out.println("hello git4");
     }
 }
